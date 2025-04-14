4b0a7d0ac14c081201bb8ccfcb73e1bd5117b8ab
Priyanshu-Iron <ps2894944@gmail.com>
2025-04-14 22:07:10 +0530

    bug fix:

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -14,13 +14,13 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 // Public routes
-router.route("/register").post(
-    upload.fields([
-        { name: "avatar", maxCount: 1 },
-        { name: "coverImage", maxCount: 1 }
-    ]),
-    registerUser
-);
+// router.route("/register").post(
+//     upload.fields([
+//         { name: "avatar", maxCount: 1 },
+//         { name: "coverImage", maxCount: 1 }
+//     ]),
+//     registerUser
+// );
 router.route("/login").post(loginUser);
 router.route("/refresh-token").post(refreshAccessToken);
 
